dc9ded4abd772ef2bc4bd818de20207e89fa2a61
Farhadcode <farhad.ahmed.aust@gmail.com>
2022-02-14 22:45:54 +0600

    all done server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const { MongoClient } = require('mongodb');
-const ObjectId = require('mongodb').ObjectID;
+const ObjectId = require('mongodb').ObjectId;
 const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -22,14 +22,87 @@ async function run() {
     try {
         await client.connect();
         // console.log('database connn');
-        const database = client.db("heroRider");
-        const usersCollection = database.collection('usersData');
+        const database = client.db("travelAgency");
+        // const usersCollection = database.collection('usersData');
+        const toureCollection = database.collection('alltours')
+        const BookingCollection = database.collection('Bookingtours')
 
-        // get API
-        app.post('/users', async (req, res) => {
-            const user = req.body;
-            const result = await usersCollection.insertOne(user);
+
+
+        // get all tours date
+
+        app.get('/tours', async (req, res) => {
+            const cursor = toureCollection.find({});
+            const tours = await cursor.toArray();
+            res.send(tours)
+        })
+
+        // get single tours data 
+
+        app.get('/tours/:id', async (req, res) => {
+            const id = req.params.id;
+
+            const query = { _id: ObjectId(id) };
+            const tour = await toureCollection.findOne(query);
+            res.send(tour);
+        })
+
+
+
+        //post tours
+        app.post('/tourPost', async (req, res) => {
+            const newTour = req.body;
+            const result = await toureCollection.insertOne(newTour);
             res.send(result);
+        })
+
+
+        //post bookingTours
+        app.post('/bookingTours', async (req, res) => {
+            const booking = req.body;
+            const result = await BookingCollection.insertOne(booking);
+            res.send(result);
+        })
+
+        // all booking
+        app.get('/allBooking', async (req, res) => {
+            const cursor = BookingCollection.find({});
+            const allbooks = await cursor.toArray();
+            res.send(allbooks)
+        })
+        // get My myBooking 
+
+        app.get("/myBooking/:email", async (req, res) => {
+            const result = await BookingCollection.find({
+                email: req.params.email,
+            }).toArray();
+            res.send(result);
+        });
+
+        // delate my booking 
+        app.delete('/deleteBooking/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log("deleta", id);
+            const query = { _id: ObjectId(id) }
+            const result = await BookingCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        // update booking status
+
+        app.put('/updateStatusBooking/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const updatedBooking = req.body;
+            const query = { _id: ObjectId(id) };
+            const options = { upsert: true }
+            const updatedDoc = {
+                $set: {
+                    status: updatedBooking.status
+                },
+            };
+            const result = await BookingCollection.updateOne(query, updatedDoc, options)
+            res.send(result)
         })
     }
     finally {
@@ -41,9 +114,9 @@ run().catch(console.dir);
 
 
 app.get('/', function (req, res) {
-    res.send('heroRider server')
+    res.send('travel agency')
 })
 
 app.listen(port, () => {
-    console.log(`heroRider server ${port}`);
+    console.log(`travelagency ${port}`);
 })
